7f52102f1fcee1f08e4867a15d244414e39f8f43
kimtaesung <kim25576@naver.com>
2025-01-16 17:06:38 +0900

    initial commit with 2024 solution

---
+++ link_network_1922/link_network_1922.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int* parent;
+int* union_find;
+int find(int a) {
+	
+}
+class line {
+public:
+	int x, y, weight;
+	line(int a, int b,int c) :x(a), y(b), weight(c){}
+};
+int main() {
+	int n, m,a,b,c;
+	vector<line> l;
+	scanf_s("%d %d",&n,&m);
+	l = new vector<line>[m];
+	for (int i = 0; i < m; i++) {
+		scanf_s("%d %d %d", &a, &b, &c);
+		l[i] = line(a, b, c);
+	}
+}
